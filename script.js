55755a7fe353a48e82b9628a64c30b3d376f9b35
dee <178827694+dee797@users.noreply.github.com>
2024-09-26 15:26:13 -0600

    Create JS with factory functions

---
+++ script.js
@@ -0,0 +1,22 @@
+const gameboard = (function() {
+    const grid = [["","",""],["","",""],["","",""]];
+
+    const setMarker = (player, selection) => {
+
+    }
+})();
+
+
+function createPlayer(name) {
+
+}
+
+
+const game = (function() {
+
+})();
+
+
+const displayController = (function() {
+
+})();
