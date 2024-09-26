06d80111c98d583faf001c28073c72b169875579
dee <178827694+dee797@users.noreply.github.com>
2024-09-26 16:02:23 -0600

    Add properties, methods for gameboard and player

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const gameboard = (function() {
-    const grid = [["","",""],["","",""],["","",""]];
+    const grid = ["","","","","","","","",""];
 
     const setMarker = (player, selection) => {
-
+        if (selection !== "") grid[selection] = player.marker;
     }
+
+    return { grid, setMarker };
 })();
 
 
-function createPlayer(name) {
+function player(name="") {
+    let marker = name !== "computer" ? "x" : "o";
+    let markerCount = 0;
 
+    const incMarkerCount = () => {
+        if (markerCount < 3) markerCount++;
+    };
+
+    return { marker, incMarkerCount };
 }
 
 
@@ -18,5 +27,7 @@ const game = (function() {
 
 
 const displayController = (function() {
+    const spaces = document.querySelectorAll(".space");
+
 
 })();
