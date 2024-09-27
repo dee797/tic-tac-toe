1eb6dded1d58c98d0ffd1d08a12d3df5b6cb5bb3
dee <178827694+dee797@users.noreply.github.com>
2024-09-27 13:20:18 -0600

    Add logic for checking the winner each turn

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const gameboard = (function() {
-    const grid = ["","","","","","","","",""];
+    let grid = ["","","",
+                "","","",
+                "","",""];
 
     const setMarker = (player, selection) => {
-        selection !== "" ? 
+        grid[selection] === "" ? 
         grid[selection] = player.marker : 
         alert("This spot has already been taken.");
     }
@@ -13,7 +15,7 @@ const gameboard = (function() {
 
 
 function player(playerNum, name="") {
-    let marker = playerNum === "1" ? "x" : "o";
+    let marker = playerNum === 1 ? "x" : "o";
     let markerCount = 0;
 
     const incMarkerCount = () => {
@@ -27,22 +29,49 @@ function player(playerNum, name="") {
 
 const game = (function() {
     
-    const player1 = player(1, prompt("Player 1 - You will have 'x' markers.\n Please enter your name:"));
-    const player2 = player(2, prompt("Player 2 - You will have 'o' markers.\n Please enter your name:"));
+    const player1 = player(1, prompt("Player 1 - You will go first and have 'x' markers.\nPlease enter your name:"));
+    const player2 = player(2, prompt("Player 2 - You will have 'o' markers.\nPlease enter your name:"));
 
     const spaces = document.querySelectorAll(".space");
 
-    const turnCount = 1;
+    let turnCount = 1;
+    let currentplayer;
     
+    const checkWin = () => {
+        if (gameboard.grid.slice(0,3).every(value => value === currentplayer.marker) ||
+            gameboard.grid.slice(3,6).every(value => value === currentplayer.marker) ||
+            gameboard.grid.slice(6).every(value => value === currentplayer.marker) ||
+
+            [gameboard.grid[0], gameboard.grid[3], gameboard.grid[6]].every(value => value === currentplayer.marker) ||
+            [gameboard.grid[1], gameboard.grid[4], gameboard.grid[7]].every(value => value === currentplayer.marker) ||
+            [gameboard.grid[2], gameboard.grid[5], gameboard.grid[8]].every(value => value === currentplayer.marker) ||
+
+            [gameboard.grid[0], gameboard.grid[4], gameboard.grid[8]].every(value => value === currentplayer.marker) ||
+            [gameboard.grid[2], gameboard.grid[4], gameboard.grid[6]].every(value => value === currentplayer.marker)) {
+
+                alert(`Player ${currentplayer.playerNum} wins!`);
+            }
+        else if (gameboard.grid.every(value => value !== "")) {
+                alert("It's a tie!");
+        } else {
+                turnCount++;
+        }
+    };
+
 
     for (const space of spaces) {
         space.addEventListener("click", () => {
             const spaceID = space.id;
+
             turnCount % 2 !== 0 ? 
-            gameboard.setMarker(player1, spaceID) :
-            gameboard.setMarker(player2, spaceID);
+            currentplayer = player1 :
+            currentplayer = player2;
+
+            gameboard.setMarker(currentplayer, spaceID);
+
             displayController.displayMarker();
-            turnCount++;
+
+            checkWin();
         });
     }
 
@@ -56,17 +85,17 @@ const displayController = (function() {
     const displayMarker = () => {
 
         for (let i = 0; i < gameboard.grid.length; i++) {
-            const bigX = "\u00D7";
 
-            gameboard.grid[i] === "x" ?
-            game.spaces[i].textContent = bigX :
-            game.spaces[i].textContent = "O";
+            //set markers based on gameboard.grid elements
+            if (gameboard.grid[i] === "x") game.spaces[i].textContent = "X";
+            else if (gameboard.grid[i] === "o") game.spaces[i].textContent = "O";
+            
         }
-        //set markers based on gameboard.grid
     }
 
     return { displayMarker };
 })();
 
-// IIFE instantly executes code in function,
+
+// IIFE instantly executes code within factory function,
 // no need to instantiate object outside of definition
