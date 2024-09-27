5f0cbe0b453c55d9f62da02c0383c9a15ea5e35f
dee <178827694+dee797@users.noreply.github.com>
2024-09-27 11:30:07 -0600

    Add logic for game and displayController

--- script.js
+++ script.js
@@ -2,32 +2,71 @@ const gameboard = (function() {
     const grid = ["","","","","","","","",""];
 
     const setMarker = (player, selection) => {
-        if (selection !== "") grid[selection] = player.marker;
+        selection !== "" ? 
+        grid[selection] = player.marker : 
+        alert("This spot has already been taken.");
     }
 
     return { grid, setMarker };
 })();
 
 
-function player(name="") {
-    let marker = name !== "computer" ? "x" : "o";
+
+function player(playerNum, name="") {
+    let marker = playerNum === "1" ? "x" : "o";
     let markerCount = 0;
 
     const incMarkerCount = () => {
         if (markerCount < 3) markerCount++;
     };
 
-    return { marker, incMarkerCount };
+    return { playerNum, name, marker, incMarkerCount };
 }
 
 
+
 const game = (function() {
+    
+    const player1 = player(1, prompt("Player 1 - You will have 'x' markers.\n Please enter your name:"));
+    const player2 = player(2, prompt("Player 2 - You will have 'o' markers.\n Please enter your name:"));
+
+    const spaces = document.querySelectorAll(".space");
+
+    const turnCount = 1;
+    
+
+    for (const space of spaces) {
+        space.addEventListener("click", () => {
+            const spaceID = space.id;
+            turnCount % 2 !== 0 ? 
+            gameboard.setMarker(player1, spaceID) :
+            gameboard.setMarker(player2, spaceID);
+            displayController.displayMarker();
+            turnCount++;
+        });
+    }
+
+    return { player1, player2, spaces };
 
 })();
+
 
 
 const displayController = (function() {
-    const spaces = document.querySelectorAll(".space");
+    const displayMarker = () => {
 
+        for (let i = 0; i < gameboard.grid.length; i++) {
+            const bigX = "\u00D7";
 
+            gameboard.grid[i] === "x" ?
+            game.spaces[i].textContent = bigX :
+            game.spaces[i].textContent = "O";
+        }
+        //set markers based on gameboard.grid
+    }
+
+    return { displayMarker };
 })();
+
+// IIFE instantly executes code in function,
+// no need to instantiate object outside of definition
